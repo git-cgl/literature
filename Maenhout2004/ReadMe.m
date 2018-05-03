081be2d3ea18921f2bd30a356b26c284487b2df9
Liu Peixin <liupx.13@sem.tsinghua.edu.cn>
2018-05-03 11:02:52 +0800

    Update and rename ReadMe to ReadMe.m

--- Maenhout2004/ReadMe.m
+++ Maenhout2004/ReadMe.m
@@ -1,2 +1,6 @@
+# Bayesian neural network papers
+
 ## Orginal
 1. [A Practical Bayesian Framework for Backpropagation Networks](https://authors.library.caltech.edu/13793/1/MACnc92b.pdf)
+
+2. [Bayesian learning for neural networks](http://www.csri.utoronto.ca/~radford/ftp/thesis.pdf)
